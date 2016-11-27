344d658b5c65aab217659fb79f2d8a04bc2c21a9
JohannisK <johannis.kragt@gmail.com>
2016-11-27 17:38:30 +0100

    Added some more classes to the test, for testing more root elements

--- test/src/main/java/nl/johannisk/finalizer/test/Test.java
+++ test/src/main/java/nl/johannisk/finalizer/test/Test.java
@@ -12,13 +12,20 @@ public class Test {
     String t1 = "";
     static String t2 = "";
     @MutableVar String t3 = "";
+
+
     public static void main(String... args) {
         new Test();
         new Test2();
+        new Test3();
     }
 
     public Test () {
         t3 = "test";
+
+        for(@MutableVar int i = 0; i < 5; i++) {
+            t3 = "" + i;
+        }
     }
 }
 
@@ -29,6 +36,24 @@ class Test2{
     @MutableVar String t3 = "";
 
     public Test2 () {
+        t3 = "test";
+
+        for(@MutableVar int i = 0; i < 5; i++) {
+            t3 = "" + i;
+        }
+    }
+}
+
+class Test3{
+    String t1 = "";
+    static String t2 = "";
+    String t3 = "";
+
+    public Test3 () {
         t1 = "test";
+
+        for(int i = 0; i < 5; i++) {
+            t3 = "" + i;
+        }
     }
 }
